1559f048c99b1ad86e114bc899ec15db3be9bfcf
afei <165532911+afeimod@users.noreply.github.com>
2026-07-27 22:43:48 +0800

    Delete app directory

--- app/src/main/java/com/flashbox/app/web/KeyDispatchBridge.kt
+++
@@ -1,7 +0,0 @@
-package com.flashbox.app.web
-
-/** Bridge for dispatching synthetic keyboard events into the WebView content. */
-interface KeyDispatchBridge {
-    fun sendKeyDown(key: String, code: Int)
-    fun sendKeyUp(key: String, code: Int)
-}
